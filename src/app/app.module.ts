b94e6e69a4ef7649dc38cbd768c7f892407b6c03
Yiyi Chen <chen.yiyi@hotmail.it>
2020-12-19 11:01:02 +0100

    Install angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
