a5f3f8f09efb4a24e1100964efa184307b71d5aa
Yiyi Chen <chen.yiyi@hotmail.it>
2020-12-29 23:03:49 +0100

    Change api config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
 	production: false,
 	weatherAPI: {
-		appKey: '33028dc9bamshc7a6a58130988b9p1e08c0jsnf8cb9fa80510',
+		apiKey: '33028dc9bamshc7a6a58130988b9p1e08c0jsnf8cb9fa80510',
 		url: 'https://community-open-weather-map.p.rapidapi.com/weather',
 	},
 	mapAPI: {
-		appKey: 'AIzaSyC0UfnIPXjVVKf8PizdS0VRJL0hsu4JL_A',
-		geocodeUrl: 'https://maps.googleapis.com/maps/api/geocode/json',
+		apiKey: '1vFZKYF-Lkd-1Cgxe1OtCRnTyCHrqLlG68zrzRNyeiM',
+		url: 'https://geocode.search.hereapi.com/v1',
 	},
 };
 
