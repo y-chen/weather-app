f2dac1210b3782ba245c5a0ba245157d1cfce08e
Yiyi Chen <chen.yiyi@hotmail.it>
2020-12-29 22:07:21 +0100

    Third party api config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,14 @@
 
 export const environment = {
 	production: false,
+	weatherAPI: {
+		appKey: '33028dc9bamshc7a6a58130988b9p1e08c0jsnf8cb9fa80510',
+		url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+	},
+	mapAPI: {
+		appKey: 'AIzaSyC0UfnIPXjVVKf8PizdS0VRJL0hsu4JL_A',
+		geocodeUrl: 'https://maps.googleapis.com/maps/api/geocode/json',
+	},
 };
 
 /*
