16aa9265b6ff487849e9c7c9741d16d4afc5cc5d
Yiyi Chen <chen.yiyi@hotmail.it>
2020-12-31 16:52:11 +0100

    Add CoreModule import protection

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -30,4 +30,10 @@ import { OpenWeatherMapService } from '@wa/app/core/services/open-weather-map/op
 		OpenWeatherMapService,
 	],
 })
-export class CoreModule {}
+export class CoreModule {
+	constructor(@Optional() @SkipSelf() core: CoreModule) {
+		if (core) {
+			throw new Error('You should import core module only in the root module');
+		}
+	}
+}
