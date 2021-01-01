b9bcef167fa883c8abd94e8c93cd1454e1c52298
Yiyi Chen <chen.yiyi@hotmail.it>
2021-01-01 09:04:32 +0100

    Translate forecast on language switch

--- src/app/features/home/components/forecast/forecast.component.ts
+++ src/app/features/home/components/forecast/forecast.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { ComponentService } from '@wa/app/core/services/component/component.service';
+import { OpenWeatherService } from '@wa/app/core/services/open-weather/open-weather.service';
 import { ViewForecast } from '@wa/app/models/open-weather.model';
 
 @Component({
@@ -15,11 +17,19 @@ export class ForecastComponent implements OnInit {
 	constructor(
 		private readonly componentService: ComponentService,
 		private readonly route: ActivatedRoute,
+		private readonly translate: TranslateService,
+		private readonly openWeatherService: OpenWeatherService,
 	) {
 		this.componentService.init({ route: this.route });
 	}
 
 	async ngOnInit(): Promise<void> {
 		this.viewForecast = (await this.componentService.getResolverData('forecast')) as ViewForecast;
+
+		this.translate.onLangChange.subscribe(async () => {
+			this.viewForecast = await this.openWeatherService.getForecastByCoord({
+				coord: this.viewForecast.coord,
+			});
+		});
 	}
 }
