6c0ac3c525837b736a8e49a540e0a9b34aeadcfe
Yiyi Chen <chen.yiyi@hotmail.it>
2021-01-01 01:18:29 +0100

    Move parse weather method to service

--- src/app/core/services/open-weather-map/open-weather-map.service.ts
+++ src/app/core/services/open-weather-map/open-weather-map.service.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
+import Case from 'case';
+
 import { Injectable } from '@angular/core';
 import { ApiService } from '@wa/app/core/services/api/api.service';
 import { CultureService } from '@wa/app/core/services/culture/culture.service';
@@ -8,7 +10,7 @@ import {
 } from '@wa/app/core/services/local-storage/local-storage.service';
 import { Param } from '@wa/app/models/http.model';
 import {
-	OpenWeatherSearchParams, Weather, WeatherGroup
+	OpenWeatherSearchParams, ViewWeather, Weather, WeatherGroup
 } from '@wa/app/models/open-weather-map.model';
 import { environment } from '@wa/environments/environment';
 
@@ -49,6 +51,36 @@ export class OpenWeatherMapService {
 		params = this.appendParams(params);
 
 		return await this.api.get<Weather>(url, { params });
+	}
+
+	parseWeatherData(weather: Weather, location?: string, iconSize?: 2 | 4): ViewWeather {
+		iconSize = iconSize ? iconSize : 4;
+		const unitsType: string = this.localStorageService.get(StorageKeys.Units);
+		let temperatureUnit: string;
+		switch (unitsType) {
+			case 'standard':
+				temperatureUnit = 'K';
+				break;
+			case 'metric':
+				temperatureUnit = 'C';
+				break;
+			case 'imperial':
+				temperatureUnit = 'F';
+				break;
+			default:
+				break;
+		}
+
+		const { name, dt } = weather;
+		const { description, icon } = weather.weather[0];
+
+		return {
+			title: location || name,
+			time: this.cultureService.convertUnixTimeToLocaleTime(dt),
+			description: Case.capital(description),
+			temperature: `${Math.round(weather.main.temp)}° ${temperatureUnit}`,
+			icon: `http://openweathermap.org/img/wn/${icon}@${iconSize}x.png`,
+		};
 	}
 
 	private appendParams(params?: Param[]): Param[] {
--- src/app/shared/components/basic-weather/basic-weather.component.ts
+++ src/app/shared/components/basic-weather/basic-weather.component.ts
@@ -1,11 +1,8 @@
-import Case from 'case';
-
 import { Component, Input, OnInit } from '@angular/core';
 import { ComponentService } from '@wa/app/core/services/component/component.service';
-import { CultureService } from '@wa/app/core/services/culture/culture.service';
 import {
-	LocalStorageService, StorageKeys
-} from '@wa/app/core/services/local-storage/local-storage.service';
+	OpenWeatherMapService
+} from '@wa/app/core/services/open-weather-map/open-weather-map.service';
 import { IComponent } from '@wa/app/models/component.model';
 import { ViewWeather, Weather } from '@wa/app/models/open-weather-map.model';
 
@@ -17,55 +14,29 @@ import { ViewWeather, Weather } from '@wa/app/models/open-weather-map.model';
 })
 export class BasicWeatherComponent implements IComponent, OnInit {
 	@Input() location?: string;
-	@Input() iconSize?: 2 | 4 = 4;
+	@Input() iconSize?: 2 | 4;
 	@Input() weather: Weather;
 
 	viewData: ViewWeather;
 
 	constructor(
-		private readonly cultureService: CultureService,
 		private readonly componentService: ComponentService,
-		private readonly localStorageService: LocalStorageService,
+		private readonly openWeatherMap: OpenWeatherMapService,
 	) {
 		this.componentService.init({ localizationBasePath: 'shared.basicWeather' });
 	}
 
 	ngOnInit(): void {
 		if (this.weather) {
-			this.viewData = this.parseForecastData();
+			this.viewData = this.openWeatherMap.parseWeatherData(
+				this.weather,
+				this.location,
+				this.iconSize,
+			);
 		}
 	}
 
 	getLocalizationPath(end: string): string {
 		return this.componentService.getLocalizationPath(end);
-	}
-
-	private parseForecastData(): ViewWeather {
-		const unitsType: string = this.localStorageService.get(StorageKeys.Units);
-		let temperatureUnit: string;
-		switch (unitsType) {
-			case 'standard':
-				temperatureUnit = 'K';
-				break;
-			case 'metric':
-				temperatureUnit = 'C';
-				break;
-			case 'imperial':
-				temperatureUnit = 'F';
-				break;
-			default:
-				break;
-		}
-
-		const { name, dt } = this.weather;
-		const { description, icon } = this.weather.weather[0];
-
-		return {
-			title: this.location || name,
-			time: this.cultureService.convertUnixTimeToLocaleTime(dt),
-			description: Case.capital(description),
-			temperature: `${Math.round(this.weather.main.temp)}° ${temperatureUnit}`,
-			icon: `http://openweathermap.org/img/wn/${icon}@${this.iconSize}x.png`,
-		};
 	}
 }
