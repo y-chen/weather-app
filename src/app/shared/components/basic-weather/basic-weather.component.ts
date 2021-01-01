02c67299fcb7ffbfa90ddabdc0f93be08f90b0a5
Yiyi Chen <chen.yiyi@hotmail.it>
2021-01-01 01:37:00 +0100

    Separate weather card and basic weather components

--- src/app/shared/components/basic-weather/basic-weather.component.ts
+++ src/app/shared/components/basic-weather/basic-weather.component.ts
@@ -1,10 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ComponentService } from '@wa/app/core/services/component/component.service';
-import {
-	OpenWeatherMapService
-} from '@wa/app/core/services/open-weather-map/open-weather-map.service';
-import { IComponent } from '@wa/app/models/component.model';
-import { ViewWeather, Weather } from '@wa/app/models/open-weather-map.model';
+import { ViewWeather } from '@wa/app/models/open-weather-map.model';
 
 @Component({
 	selector: 'wa-basic-weather',
@@ -12,31 +8,6 @@ import { ViewWeather, Weather } from '@wa/app/models/open-weather-map.model';
 	styleUrls: ['./basic-weather.component.scss'],
 	providers: [ComponentService],
 })
-export class BasicWeatherComponent implements IComponent, OnInit {
-	@Input() weather: Weather;
-	@Input() location?: string;
-	@Input() iconSize?: 2 | 4;
-
-	viewData: ViewWeather;
-
-	constructor(
-		private readonly componentService: ComponentService,
-		private readonly openWeatherMap: OpenWeatherMapService,
-	) {
-		this.componentService.init({ localizationBasePath: 'shared.basicWeather' });
-	}
-
-	ngOnInit(): void {
-		if (this.weather) {
-			this.viewData = this.openWeatherMap.parseWeatherData(
-				this.weather,
-				this.location,
-				this.iconSize,
-			);
-		}
-	}
-
-	getLocalizationPath(end: string): string {
-		return this.componentService.getLocalizationPath(end);
-	}
+export class BasicWeatherComponent {
+	@Input() viewData: ViewWeather;
 }
