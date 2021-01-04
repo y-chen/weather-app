8870413a74abeae4a5b721873c2b3fa3d4d5abb2
Yiyi Chen <chen.yiyi@hotmail.it>
2021-01-04 04:17:12 +0100

    Settings menu layout

--- src/app/shared/components/settings-menu/settings-menu.component.ts
+++ src/app/shared/components/settings-menu/settings-menu.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CultureService } from '@wa/app/core/services/culture/culture.service';
+import {
+	LocalStorageService, StorageKeys
+} from '@wa/app/core/services/local-storage/local-storage.service';
+import { Culture } from '@wa/app/models/culture.model';
 
 @Component({
 	selector: 'wa-settings-menu',
@@ -6,7 +11,35 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./settings-menu.component.scss'],
 })
 export class SettingsMenuComponent implements OnInit {
-	constructor() {}
+	cultures: Culture[];
+	currentLang: string;
+	currentUnit: string;
 
-	ngOnInit(): void {}
+	constructor(private readonly cultureService: CultureService, private readonly localStorageService: LocalStorageService) {}
+
+	ngOnInit(): void {
+		this.cultures = this.cultureService.getAvailableCultures();
+		this.currentLang = this.cultureService.getCulture().language;
+
+		const storedUnit = this.localStorageService.get(StorageKeys.Units);
+		this.currentUnit = storedUnit ? storedUnit : 'metric';
+	}
+
+	setCulture(event: MouseEvent, culture: Culture): void {
+		event.stopPropagation();
+
+		if (this.currentLang !== culture.language) {
+			this.cultureService.setCulture(culture);
+			this.currentLang = culture.language;
+		}
+	}
+
+	setUnits(event: MouseEvent, unit: string): void {
+		event.stopPropagation();
+
+		if (unit !== this.currentUnit) {
+			this.localStorageService.set(StorageKeys.Units, unit);
+			this.currentUnit = unit;
+		}
+	}
 }
