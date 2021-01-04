f2e3ac69de9a7c570467ee3a101391bb57543d2b
Yiyi Chen <chen.yiyi@hotmail.it>
2021-01-04 14:14:54 +0100

    Remove unused import

--- src/app/shared/components/error-page/error-page.component.ts
+++ src/app/shared/components/error-page/error-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { ComponentService } from '@wa/app/core/services/component/component.service';
 
 @Component({
