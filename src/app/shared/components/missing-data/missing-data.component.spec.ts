45d52ddbe91f4ed9bc3592c4406291211800dede
Yiyi Chen <chen.yiyi@hotmail.it>
2021-01-05 21:16:35 +0100

    Missing data component tests

--- src/app/shared/components/missing-data/missing-data.component.spec.ts
+++ src/app/shared/components/missing-data/missing-data.component.spec.ts
@@ -1,4 +1,4 @@
-import { createHostFactory, SpectatorHost } from '@ngneat/spectator';
+import { byText, createHostFactory, SpectatorHost } from '@ngneat/spectator';
 import { MissingDataComponent } from '@wa/app/shared/components/missing-data/missing-data.component';
 
 describe('MissingDataComponent', () => {
@@ -11,9 +11,24 @@ describe('MissingDataComponent', () => {
 	it('should create', () => {
 		host = createHost('<wa-missing-data></wa-missing-data>');
 
-		const search = host.queryHost('wa-missing-data');
+		const missingData = host.queryHost('wa-missing-data');
 
 		expect(host).toExist();
-		expect(search).toExist();
+		expect(missingData).toExist();
+	});
+
+	it('should display title and message as expected', () => {
+		const expectedTitle = 'Title';
+		const expectedMessage = 'Message';
+
+		host = createHost('<wa-missing-data [title]="expectedTitle" [message]="expectedMessage"></wa-missing-data>', {
+			hostProps: { expectedTitle, expectedMessage },
+		});
+
+		const title = host.query(byText(expectedTitle));
+		const message = host.query(byText(expectedMessage));
+
+		expect(title).toBeDefined();
+		expect(message).toBeDefined();
 	});
 });
