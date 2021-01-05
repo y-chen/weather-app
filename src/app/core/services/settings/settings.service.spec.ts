75fe60918c5422803b1464c2ea596760fd949fa5
Yiyi Chen <chen.yiyi@hotmail.it>
2021-01-05 01:54:13 +0100

    Settings service getUnit tests

--- src/app/core/services/settings/settings.service.spec.ts
+++ src/app/core/services/settings/settings.service.spec.ts
@@ -1,15 +1,56 @@
-import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
+import { mock, MockProxy, mockReset } from 'jest-mock-extended';
 
-import { SettingsService } from './settings.service';
+import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
+import { LocalStorageService, StorageKeys } from '@wa/app/core/services/local-storage/local-storage.service';
+import { SettingsService } from '@wa/app/core/services/settings/settings.service';
+import { Units } from '@wa/app/models/open-weather.model';
 
 describe('SettingsService', () => {
 	let spectator: SpectatorService<SettingsService>;
+	let localStorageServiceMock: MockProxy<LocalStorageService>;
 
-	const createService = createServiceFactory(SettingsService);
+	const createService = createServiceFactory({
+		service: SettingsService,
+		mocks: [LocalStorageService],
+	});
 
-	beforeEach(() => (spectator = createService()));
+	beforeEach(() => {
+		localStorageServiceMock = mock<LocalStorageService>();
+
+		mockReset(localStorageServiceMock);
+
+		spectator = createService({
+			providers: [{ provide: LocalStorageService, useValue: localStorageServiceMock }],
+		});
+	});
 
 	it('should be created', () => {
 		expect(spectator.service).toBeDefined();
+	});
+
+	describe('getUnit', () => {
+		it('should call LocalStorageService.get with expected key', () => {
+			spectator.service.getUnit();
+
+			expect(localStorageServiceMock.get).toHaveBeenCalledWith(StorageKeys.Units);
+		});
+
+		it('should return correct unit parsing LocalStorageService.get result', () => {
+			const expectedUnit: Units = Units.Imperial;
+
+			localStorageServiceMock.get.calledWith(StorageKeys.Units).mockReturnValue(expectedUnit);
+
+			const unit: Units = spectator.service.getUnit();
+
+			expect(unit).toEqual(expectedUnit);
+		});
+
+		it('should return default Metric unit when LocalStorageService.get returns an invalid result', () => {
+			localStorageServiceMock.get.calledWith(StorageKeys.Units).mockReturnValue(null);
+
+			const unit: Units = spectator.service.getUnit();
+
+			expect(unit).toEqual(Units.Metric);
+		});
 	});
 });
