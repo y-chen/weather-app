af39f25cae785d2be20af3c8e4c0c9f1190fd4e7
Yiyi Chen <chen.yiyi@hotmail.it>
2021-01-07 06:59:06 +0100

    Prevent making call if there's no forecast

--- src/app/features/main/components/forecast/forecast.component.ts
+++ src/app/features/main/components/forecast/forecast.component.ts
@@ -58,9 +58,11 @@ export class ForecastComponent implements IComponent, OnInit {
 
 	private refreshTranslations(): void {
 		const refreshViewData: () => Promise<void> = async (): Promise<void> => {
-			this.forecast = await this.openWeatherService.getForecastByCoord({
-				coord: this.forecast.coord,
-			});
+			if (this.forecast.coord) {
+				this.forecast = await this.openWeatherService.getForecastByCoord({
+					coord: this.forecast.coord,
+				});
+			}
 		};
 
 		this.cultureService.onLangChange(refreshViewData);
