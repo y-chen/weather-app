d04506599245fdbb5833adae3e8f1f69f3223663
Yiyi Chen <chen.yiyi@hotmail.it>
2021-01-09 14:02:27 +0100

    Footer layout

--- src/app/shared/components/shell/footer/footer.component.ts
+++ src/app/shared/components/shell/footer/footer.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Author } from '@wa/app/models/environment.model';
+import { environment } from '@wa/environments/environment';
 
 @Component({
-  selector: 'wa-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+	selector: 'wa-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnChanges, OnInit {
+	@Input() open?: boolean = true;
 
-  constructor() { }
+	@HostBinding('attr.class') hostClass: string;
 
-  ngOnInit(): void {
-  }
+	appInfo: string;
+	author: Author;
 
+	ngOnChanges(changes: SimpleChanges): void {
+		this.hostClass = changes.open.currentValue ? 'open' : 'closed';
+	}
+
+	ngOnInit(): void {
+		const { name, version, author } = environment;
+		this.appInfo = `${name} v${version}`;
+		this.author = author;
+	}
 }
