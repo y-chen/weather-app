125d19488a4e9b7fd25ab5097a7ba49a602f21cb
Yiyi Chen <chen.yiyi@hotmail.it>
2021-01-09 19:41:07 +0100

    Static about me page

--- src/app/main/components/about-me/about-me.component.ts
+++ src/app/main/components/about-me/about-me.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'wa-about-me',
-  templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+	selector: 'wa-about-me',
+	templateUrl: './about-me.component.html',
+	styleUrls: ['./about-me.component.scss'],
 })
 export class AboutMeComponent implements OnInit {
+	links = [
+		{
+			icon: 'linkedin',
+			name: 'LinkedIn',
+			url: 'https:/www.linkedin.com/in/yiyi-chen-web-developer',
+		},
+		{
+			icon: 'github',
+			name: 'Github',
+			url: 'https://github.com/y-chen',
+		},
+		{
+			icon: 'download-resume',
+			name: 'Download CV',
+			url: 'https://onedrive.live.com/download?cid=D019FA525DBE651D&resid=D019FA525DBE651D%214751&authkey=AEgnuD75ZkHhF3A&em=2',
+		},
+	];
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
