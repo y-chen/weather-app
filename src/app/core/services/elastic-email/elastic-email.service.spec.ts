ea4b9744fbcd60243f5d4c6fabf1f4dfd49232c6
Yiyi Chen <chen.yiyi@hotmail.it>
2021-01-10 02:08:57 +0100

    Fix test

--- src/app/core/services/elastic-email/elastic-email.service.spec.ts
+++ src/app/core/services/elastic-email/elastic-email.service.spec.ts
@@ -1,4 +1,5 @@
 import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
+import { MasterMock } from '@wa/app/common/master-mock';
 
 import { ElasticEmailService } from './elastic-email.service';
 
@@ -8,7 +9,11 @@ describe('ElasticEmailService', () => {
 	const createService = createServiceFactory(ElasticEmailService);
 
 	beforeEach(() => {
-		spectator = createService();
+		const { configServiceProvider } = new MasterMock().mockConfig();
+
+		spectator = createService({
+			providers: [configServiceProvider],
+		});
 	});
 
 	it('should be created', () => {
