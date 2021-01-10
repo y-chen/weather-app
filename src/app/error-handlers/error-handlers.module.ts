a5ed6cca1c5e6d30d8d57aacefb6c31e33ac1db1
Yiyi Chen <chen.yiyi@hotmail.it>
2021-01-10 15:06:43 +0100

    Move interceptor to module root

--- src/app/error-handlers/error-handlers.module.ts
+++ src/app/error-handlers/error-handlers.module.ts
@@ -1,6 +1,6 @@
 import { ErrorHandler, NgModule } from '@angular/core';
 
-import { GlobalErrorHandler } from './error-handlers/global/global.error-handler';
+import { GlobalErrorHandler } from './global/global.error-handler';
 
 @NgModule({
 	providers: [{ provide: ErrorHandler, useClass: GlobalErrorHandler }],
